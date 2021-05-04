2b3e3534460076289a3b44be325d277c24f0b21a
alvindcaesar <me@alvindcaesar.com>
2021-05-04 16:24:08 +0800

    Added functionality

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded' , () => {
 
     let button = document.getElementById('button');
-    let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    let code = document.querySelector('.code')
+    let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let code = document.querySelector('.code');
+    let history_board = document.querySelector('.code-record');
     
     button.addEventListener('click', generateCode);
     function generateCode() {
@@ -11,9 +12,28 @@ document.addEventListener('DOMContentLoaded' , () => {
             sixNumbers += number[getRandomCode()]
             code.innerHTML = sixNumbers.split('').join(' ')
         }
-        console.log(sixNumbers)
+
+        getRecord(sixNumbers)
+        // console.log(sixNumbers)
     }
     function getRandomCode(){
         return Math.floor(Math.random() * number.length)
     }
+    function getRecord(sixNumbers){
+        let element = document.createElement('div')
+        element.classList.add('code-record')
+        element.innerHTML = `<p class="record">${sixNumbers}</p>
+                 <button class="remove-button type="button" name="button">delete plis</button>
+                </div>`
+        
+        history_board.appendChild(element)
+        let remove_button = element.querySelector('.remove-button');
+        remove_button.addEventListener('click', removeRecord);
+
+    }
+    function removeRecord(e){
+        let record = e.currentTarget.parentElement
+        history_board.removeChild(record)
+    }
+
 })
