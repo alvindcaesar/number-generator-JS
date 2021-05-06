d0b3b0db5151371b10792e4a6c4ad1a8186f5ffa
alvindcaesar <me@alvindcaesar.com>
2021-05-06 12:26:15 +0800

    Code improvement

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ document.addEventListener('DOMContentLoaded' , () => {
     let clear_button = document.getElementById('clear-button');
     let restore_records = [];
     let restore_deleted_records = document.getElementById('restore-button');
+    let timer_display = document.querySelector('.timer');
+    let timer = 0;
     
     clear_button.addEventListener('click', removeAllRecords);
-    
     button.addEventListener('click', generateCode);
-
     restore_deleted_records.addEventListener('click', restoreRecords);
-
 
     function generateCode() {
         let sixNumbers = '';
@@ -23,11 +22,12 @@ document.addEventListener('DOMContentLoaded' , () => {
         }
 
         getRecord(sixNumbers)
-        
     }
+
     function getRandomCode(){
         return Math.floor(Math.random() * number.length)
     }
+
     function getRecord(sixNumbers){
         let element = document.createElement('div')
         element.classList.add('code-record')
@@ -37,15 +37,15 @@ document.addEventListener('DOMContentLoaded' , () => {
         history_board.appendChild(element)
         let remove_button = element.querySelector('.remove-button');
         remove_button.addEventListener('click', removeRecord);
-
     }
+
     function removeRecord(e){
         let record = e.currentTarget.parentElement
         history_board.removeChild(record)
+        startTimer()
         storeDeletedRecord(record)
     }
 
-   
     function removeAllRecords() {
         let records = document.querySelectorAll('.code-record');
         records.forEach( function (record) {
@@ -54,7 +54,9 @@ document.addEventListener('DOMContentLoaded' , () => {
         storeDeletedRecord(record)
         
         });
-
+        restore_deleted_records.style.display = 'inline';
+        startTimer();
+        timer = 10;
     }
 
     function storeDeletedRecord(record){
@@ -66,6 +68,27 @@ document.addEventListener('DOMContentLoaded' , () => {
             history_board.appendChild(record)
 
         });
+        restore_deleted_records.style.display = "none";
+        timer_display.style.display = "inline";
+        timer = 0;
+        restore_records = [];
     }
 
+    function startTimer(){
+        restore_deleted_records.style.display = "inline";
+        timer_display.style.display = "inline";
+
+        let runTimer = setInterval(function () {
+            if (timer > 0){
+                timer -= 1
+                timer_display.innerHTML = `${timer} seconds to restore`
+            } else{
+                clearInterval(runTimer)
+                restore_deleted_records.style.display = "none";
+                timer_display.style.display = "none";
+                timer = 10;
+                restore_records = [];
+            }
+        }, 1000);
+    }
 })
